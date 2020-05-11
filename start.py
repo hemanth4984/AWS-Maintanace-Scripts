5f13b494a2bb978d1019328cf13e11c9874576be
hemanth4984 <37407977+hemanth4984@users.noreply.github.com>
2020-05-11 19:48:07 +0530

    Update start.py

--- start.py
+++ start.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import boto3
 region = 'ap-south-1'
-instances = ['i-05c70118a881cd854', 'i-04d753c4a5d3e66f2']
+instances = ['i-*********', 'i-*************']
 ec2 = boto3.client('ec2', region_name=region)
 ec2.start_instances(InstanceIds=instances)
 print('started your instances: ' + str(instances))
